be325b0f1e1f624c92d41fe508aab07bf68742cc
Xinyue <858937756@qq.com>
2025-05-01 19:42:29 +0100

    Initial commit

---
+++ test/ds-assignment2-photo-gallery.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DsAssignment2PhotoGallery from '../lib/ds-assignment2-photo-gallery-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ds-assignment2-photo-gallery-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DsAssignment2PhotoGallery.DsAssignment2PhotoGalleryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
