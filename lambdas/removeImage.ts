3a735d417b1ada0ea39a654a209811ea1ccec5c6
Xinyue <858937756@qq.com>
2025-05-03 14:01:55 +0100

    Modify removeImage

--- lambdas/removeImage.ts
+++ lambdas/removeImage.ts
@@ -4,20 +4,24 @@ import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 const s3 = new S3Client({ region: process.env.REGION });
 
 export const handler: SQSHandler = async (event) => {
+  console.log("RemoveImageFn received event:", JSON.stringify(event));
+
   for (const record of event.Records) {
-    const body = JSON.parse(record.body);
-    const s3Info = body.Records?.[0]?.s3;
-    if (s3Info) {
-      const bucket = s3Info.bucket.name;
-      const key = decodeURIComponent(s3Info.object.key.replace(/\+/g, " "));
-      try {
-        await s3.send(
-          new DeleteObjectCommand({ Bucket: bucket, Key: key })
-        );
+    try{
+    const snsMessage = JSON.parse(record.body);
+    const s3Event = JSON.parse(snsMessage.Message);
+  
+    for (const s3Record of s3Event.Records) {
+      const bucket = s3Record.s3.bucket.name;
+      const key = decodeURIComponent(
+        s3Record.s3.object.key.replace(/\+/g, " ")
+      );
+
+        await s3.send(new DeleteObjectCommand({ Bucket: bucket, Key: key }));
         console.log(`Deleted invalid file: ${key}`);
-      } catch (err) {
-        console.error(`Failed to delete ${key}:`, err);
       }
+    } catch (err) {
+      console.error("RemoveImageFn Error:", err);
     }
   }
 };
